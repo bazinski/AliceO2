9fe410a623dae81fb91f2a06727f5d701a9c7097
Sean <hamiltonthomas@gmail.com>
2020-07-24 13:07:55 +0200

    cru data to tracklets and digits and compress and more

---
+++ Detectors/TRD/reconstruction/include/TRDReconstruction/DataReaderTask.h
@@ -0,0 +1,62 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   DataReaderTask.h
+/// @author Sean Murray
+/// @brief  TRD epn task to read incoming data
+
+#ifndef O2_TRD_DATAREADERTASK
+#define O2_TRD_DATAREADERTASK
+
+#include "Framework/Task.h"
+#include "Framework/DataProcessorSpec.h"
+#include "TRDReconstruction/CruRawReader.h"
+#include "TRDReconstruction/CompressedRawReader.h"
+#include "DataFormatsTRD/Tracklet64.h"
+#include "DataFormatsTRD/TriggerRecord.h"
+#include "TRDBase/Digit.h"
+//#include "DataFormatsTRD/FlpStats.h"
+
+#include <fstream>
+
+using namespace o2::framework;
+
+namespace o2::trd
+{
+
+class DataReaderTask : public Task
+{
+ public:
+  DataReaderTask() = default;
+  ~DataReaderTask() override = default;
+  void init(InitContext& ic) final;
+  void sendData(ProcessingContext& pc);
+  void run(ProcessingContext& pc) final;
+
+ private:
+  CruRawReader mReader;                  // this will do the parsing, of raw data passed directly through the flp(no compression)
+  CompressedRawReader mCompressedReader; //this will handle the incoming compressed data from the flp
+                                         // in both cases we pull the data from the vectors build message and pass on.
+                                         // they will internally produce a vector of digits and a vector tracklets and associated indexing.
+                                         // TODO templatise this and 2 versions of datareadertask, instantiated with the relevant parser.
+  std::vector<Tracklet64> mTracklets;
+  std::vector<Digit> mDigits;
+  std::vector<o2::trd::TriggerRecord> mTriggers;
+  //  std::vector<o2::trd::FlpStats> mStats;
+
+  bool mVerbose{false};        // verbos output general debuggign and info output.
+  bool mDataVerbose{false};    // verbose output of data unpacking
+  bool mHeaderVerbose{false};  // verbose output of headers
+  bool mCompressedData{false}; // are we dealing with the compressed data from the flp (send via option)
+};
+
+} // namespace o2::trd
+
+#endif // O2_TRD_DATAREADERTASK
---
+++ Detectors/TRD/reconstruction/src/CompressedRawReader.cxx
@@ -0,0 +1,163 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   CompressedRawReader.h
+/// @brief  TRD raw data translator
+
+#include "TRDReconstruction/CompressedRawReader.h"
+#include "DataFormatsTRD/RawData.h"
+#include "DataFormatsTRD/Tracklet64.h"
+#include "DataFormatsTRD/CompressedHeader.h"
+#include "DetectorsRaw/RDHUtils.h"
+//#include "Headers/RAWDataHeader.h"
+#include "Headers/RDHAny.h"
+
+#include <cstring>
+#include <string>
+#include <vector>
+#include <array>
+#include <iostream>
+#include <numeric>
+
+namespace o2::trd
+{
+
+uint32_t CompressedRawReader::processHBFs()
+{
+
+  LOG(debug) << "PROCESS HBF starting at " << (void*)mDataPointer;
+
+  mDataRDH = reinterpret_cast<const o2::header::RDHAny*>(mDataPointer);
+  //mEncoderRDH = reinterpret_cast<o2::header::RDHAny*>(mEncoderPointer);
+  auto rdh = mDataRDH;
+  /* loop until RDH stop header */
+  while (!o2::raw::RDHUtils::getStop(rdh)) { // carry on till the end of the event.
+    auto headerSize = o2::raw::RDHUtils::getHeaderSize(rdh);
+    auto memorySize = o2::raw::RDHUtils::getMemorySize(rdh);
+    auto offsetToNext = o2::raw::RDHUtils::getOffsetToNext(rdh);
+    auto dataPayload = memorySize - headerSize;
+    mFEEID = o2::raw::RDHUtils::getFEEID(rdh);            //TODO change this and just carry around the curreht RDH
+    mCRUEndpoint = o2::raw::RDHUtils::getEndPointID(rdh); // the upper or lower half of the currently parsed cru 0-14 or 15-29
+    mCRUID = o2::raw::RDHUtils::getCRUID(rdh);
+    int packetCount = o2::raw::RDHUtils::getPacketCounter(rdh);
+    if (mHeaderVerbose) {
+      LOG(info) << "--- RDH open/continue detected";
+      LOG(info) << "FEEID : " << mFEEID << " Packet: " << packetCount << " sizes : header" << headerSize << " memorysize:" << memorySize << " offsettonext:" << offsetToNext << " datapayload:" << dataPayload;
+      o2::raw::RDHUtils::printRDH(rdh);
+    }
+    /* copy CRU payload to save buffer */
+    //   std::memcpy(mSaveBuffer + mSaveBufferDataSize, (char*)(rdh) + headerSize, drmPayload);
+    //   mSaveBufferDataSize += drmPayload;
+    //
+    //   we will "simply" parse on the fly with a basic state machine.
+    mDataPointer = (uint32_t*)((char*)rdh + headerSize);
+    mDataEndPointer = (const uint32_t*)((char*)rdh + offsetToNext);
+    while ((void*)mDataPointer < (void*)mDataEndPointer) { // loop to handle the case where a halfcru ends/begins mid rdh data block
+      mEventCounter++;
+      if (processBlock()) { // at this point the entire payload is in mSaveBuffer, TODO parse this incrementally, less mem foot print.
+        LOG(warn) << "processBlock return flase";
+        if (mVerbose) {
+          LOG(info) << "processBlock return flase";
+        }
+        break; // end of CRU
+      }
+      //buildCRUPayLoad(); // the rest of the hbf for the subsequent cruhalfchamber payload.
+      // TODO is this even possible if hbfs upto the stop  is for one event and each cru header is for 1 event?
+    }
+    /* move to next RDH */
+    rdh = (o2::header::RDHAny*)((char*)(rdh) + offsetToNext);
+    if (mVerbose) {
+      LOG(info) << " rdh is now at 0x" << (void*)rdh << " offset to next : " << offsetToNext;
+    }
+  }
+
+  if (o2::raw::RDHUtils::getStop(rdh)) {
+    if (mDataPointer != (const uint32_t*)((char*)rdh + o2::raw::RDHUtils::getOffsetToNext(rdh))) {
+      LOG(warn) << " at end of parsing loop and mDataPointer is on next rdh";
+    }
+    mDataPointer = (const uint32_t*)((char*)rdh + o2::raw::RDHUtils::getOffsetToNext(rdh));
+    // make sure mDataPointer is in the correct place.
+  } else {
+    mDataPointer = (const uint32_t*)((char*)rdh);
+  }
+  if (mVerbose) {
+    LOGF(debug, " at exiting processHBF after advancing to next rdh mDataPointer is %p  ?= %p", (void*)mDataPointer, (void*)mDataEndPointer);
+  }
+  o2::raw::RDHUtils::printRDH(rdh);
+
+  if (mVerbose) {
+    LOG(info) << "--- RDH close detected";
+    LOG(info) << "--- END PROCESS HBF";
+  }
+  /* move to next RDH */
+  // mDataPointer = (uint32_t*)((char*)(rdh) + o2::raw::RDHUtils::getOffsetToNext(rdh));
+
+  /* otherwise return */
+
+  return mDataEndPointer - mDataPointer;
+}
+
+bool CompressedRawReader::processBlock()
+{
+  /* process a FeeID/halfcru, 15 links */
+  //  LOG(debug) << "--- PROCESS BLOCK FeeID:" << mFEEID;
+  mCurrentLinkDataPosition = 0;
+
+  if (mVerbose) {
+    LOG(info) << "--- END PROCESS HalfCRU with state: " << mState;
+  }
+  // the  essentially inverse function of CruCompressorTask::buildOutput
+  //tracklet headers.
+  CompressedRawHeader header;
+  memcpy((char*)&header, &mDataBuffer, sizeof(CompressedRawHeader));
+  mDataBuffer += sizeof(CompressedRawHeader);
+  mDataReadIn += sizeof(CompressedRawHeader);
+  //tracklets
+  int numberoftracklets = header.size;
+  o2::InteractionRecord ir(header.bc, header.orbit);
+  gsl::span((Tracklet64*)mDataBuffer, numberoftracklets);
+  mDataReadIn += numberoftracklets * sizeof(Tracklet64);
+
+  //digits headers.
+  CompressedRawTrackletDigitSeperator tracklettrailer;
+  memcpy((char*)&tracklettrailer, &mDataBuffer, sizeof(CompressedRawTrackletDigitSeperator));
+  mDataBuffer += sizeof(CompressedRawTrackletDigitSeperator);
+  mDataReadIn += sizeof(CompressedRawTrackletDigitSeperator);
+  //digits
+  int numberofdigits = tracklettrailer.digitcount;
+  for (int digitcounter = 0; digitcounter < numberofdigits; ++digitcounter) {
+    ArrayADC timebins;
+    for (int adc = 0; adc < o2::trd::constants::TIMEBINS; adc++) {
+      timebins[adc] = mCompressedEventDigits[digitcounter][adc];
+    }
+    mEventDigits.emplace_back(mCompressedEventDigits[digitcounter].getDetector(), mCompressedEventDigits[digitcounter].getROB(),
+                              mCompressedEventDigits[digitcounter].getMCM(), mCompressedEventDigits[digitcounter].getChannel(), timebins);
+  }
+  // now send back the digits, tracklets and trigger records.
+  return true;
+}
+
+void CompressedRawReader::resetCounters()
+{
+  mEventCounter = 0;
+  mFatalCounter = 0;
+  mErrorCounter = 0;
+}
+
+void CompressedRawReader::checkSummary()
+{
+  char chname[2] = {'a', 'b'};
+
+  LOG(info) << "--- SUMMARY COUNTERS: " << mEventCounter << " events "
+            << " | " << mFatalCounter << " decode fatals "
+            << " | " << mErrorCounter << " decode errors ";
+}
+
+} // namespace o2::trd
