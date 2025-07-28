7528f864e21c83d20c06e94400cbcfe56c3a1ff3
Sean Murray <hamiltonthomas@gmail.com>
2025-07-28 14:26:45 +0200

    add tracktrd to dictionaries

--- DataFormats/Detectors/TRD/src/DataFormatsTRDLinkDef.h
+++ DataFormats/Detectors/TRD/src/DataFormatsTRDLinkDef.h
@@ -42,6 +42,7 @@
 #pragma link C++ class o2::trd::LinkToHCIDMapping + ;
 #pragma link C++ class o2::trd::ChannelInfo + ;
 #pragma link C++ class o2::trd::ChannelInfoContainer + ;
+#pragma link C++ class o2::trd::TrackTRD + ;
 #pragma link C++ struct o2::trd::PHData + ;
 #pragma link C++ struct o2::trd::PHDataHD + ;
 #pragma link C++ class o2::trd::TRDDataCountersPerTimeFrame + ;
@@ -61,7 +62,6 @@
 #pragma link C++ class std::vector < o2::trd::KrCluster> + ;
 #pragma link C++ class std::vector < o2::trd::KrClusterTriggerRecord> + ;
 #pragma link C++ class std::vector < o2::trd::DataCountersPerTrigger> + ;
-#pragma link C++ class o2::trd::TrackTRD + ;
 #pragma link C++ class std::vector < o2::trd::TrackTRD> + ;
 
 #pragma link C++ struct o2::trd::CTFHeader + ;
