64c13d9b0a3b31836e20a1005ce168650c44c234
Sean Murray <hamiltonthomas@gmail.com>
2025-07-28 17:03:29 +0200

    remove tracktrd from dictionary

--- Detectors/TRD/qc/include/TRDQC/RawDataManager.h
+++ Detectors/TRD/qc/include/TRDQC/RawDataManager.h
@@ -56,9 +56,9 @@ struct RawDataSpan {
   boost::iterator_range<std::vector<o2::trd::Digit>::iterator> digits;
   boost::iterator_range<std::vector<o2::trd::Tracklet64>::iterator> tracklets;
   boost::iterator_range<std::vector<HitPoint>::iterator> hits;
-  boost::iterator_range<std::vector<o2::DataFormats::TrackTPCITS>::iterator> tracks_itstpc;
-  boost::iterator_range<std::vector<o2::DataFormats::MatchInfoTOF>::iterator> tracks_itstpc_tof;
-  boost::iterator_range<std::vector<o2::DataFormats::MatchInfoTOF>::iterator> tracks_itstpctrd_tof;
+  boost::iterator_range<std::vector<o2::dataformats::TrackTPCITS>::iterator> tracks_itstpc;
+  boost::iterator_range<std::vector<o2::dataformats::MatchInfoTOF>::iterator> tracks_itstpc_tof;
+  boost::iterator_range<std::vector<o2::dataformats::MatchInfoTOF>::iterator> tracks_itstpctrd_tof;
   boost::iterator_range<std::vector<o2::trd::TrackTRD>::iterator> tracks_itstpctof_trd;
   boost::iterator_range<std::vector<o2::trd::TrackTRD>::iterator> tracks_itstpc_trd;
   boost::iterator_range<std::vector<o2::trd::TrackTRD>::iterator> tracks_tpc_trd;
