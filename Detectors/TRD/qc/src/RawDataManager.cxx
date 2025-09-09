93a3ae5ee2142edee5374b935d349b5073489d51
Sean Murray <hamiltonthomas@gmail.com>
2025-09-09 14:20:41 +0200

    put comparison back in

--- Detectors/TRD/qc/src/RawDataManager.cxx
+++ Detectors/TRD/qc/src/RawDataManager.cxx
@@ -1319,7 +1319,7 @@ RawDataSpan RawDataManager::getEvent()
   int counter=0;
   //LOGP(info,"mITSTPCTracks_segments.size() {} first {}  last {} before finding first and last",mITSTPCTracks_segments.size(),first,last);
   for(auto& ts : mITSTPCTracks_segments ){
-    //LOGP(info,"comparing tracksegments for extraction {} {} first {} last {} counter {}",ts.getCollisionId(),mEventNo,first,last,counter);
+    LOGP(info,"comparing tracksegments for extraction {} {} first {} last {} counter {}",ts.getCollisionId(),mEventNo,first,last,counter);
     if(ts.getCollisionId()==mEventNo && first==-1){
       first = counter;
     }
