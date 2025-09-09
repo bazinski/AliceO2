b9455fed461da8b0f7788b183e9ec7d16c08b666
Sean Murray <hamiltonthomas@gmail.com>
2025-09-09 13:24:03 +0200

    outputting itstpc tracksegments

--- Detectors/TRD/qc/src/RawDataManager.cxx
+++ Detectors/TRD/qc/src/RawDataManager.cxx
@@ -1317,6 +1317,7 @@ RawDataSpan RawDataManager::getEvent()
   int first=-1,last=9999999;
   int counter=0;
   for(auto& ts : mITSTPCTracks_TRD_segments ){
+    LOGP(info,"comparing tracksegments for extraction {} {} first {} last {}",ts.getCollisionId(),mEventNo,first,last);
     if(ts.getCollisionId()==mEventNo && first==-1){
       first = counter;
     }
