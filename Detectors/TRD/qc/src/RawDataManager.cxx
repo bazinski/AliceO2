32c32ace3d2ac064fdd21d70e366e75bcec45222
Sean Murray <hamiltonthomas@gmail.com>
2025-09-09 13:41:06 +0200

    debugging output

--- Detectors/TRD/qc/src/RawDataManager.cxx
+++ Detectors/TRD/qc/src/RawDataManager.cxx
@@ -33,7 +33,7 @@
 #include "Framework/Logger.h"
 
 using namespace o2::trd;
-constexpr bool debugprint = false;
+constexpr bool debugprint = true;
 /// comparison function to order digits by det / row / MCM / -channel
 bool comp_digit(const o2::trd::Digit& a, const o2::trd::Digit& b)
 {
@@ -1232,6 +1232,7 @@ bool RawDataManager::nextTimeFrame()
   auto tracksegmentstart = std::chrono::high_resolution_clock::now(); // measure total processing time
   //if(mTimeFrameNo>50 && mTimeFrameNo<60)buildTrackSegments(false);
   buildTrackSegments(false);
+  LOGP(info,"sorting ITSTPC track segments with size : {}",mITSTPCTracks_segments.size());
   std::stable_sort(mITSTPCTracks_segments.begin(),mITSTPCTracks_segments.end(),comp_tracksegments);
   //tracksegements are now trd trigger order.
   auto tracksegmenttime = std::chrono::high_resolution_clock::now() - tracksegmentstart;
