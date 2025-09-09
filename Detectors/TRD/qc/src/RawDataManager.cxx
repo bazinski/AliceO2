4d4e5708fd8778b313424e70c1fbd6b1433a79d2
Sean Murray <hamiltonthomas@gmail.com>
2025-09-09 14:16:31 +0200

    debugging remove erroneous _TRD_

--- Detectors/TRD/qc/src/RawDataManager.cxx
+++ Detectors/TRD/qc/src/RawDataManager.cxx
@@ -1320,7 +1320,7 @@ RawDataSpan RawDataManager::getEvent()
   int first=-1,last=9999999;
   int counter=0;
   LOGP(info,"mITSTPCTracks_segments.size() {} first {}  last {} before finding first and last",mITSTPCTracks_segments.size(),first,last);
-  for(auto& ts : mITSTPCTracks_TRD_segments ){
+  for(auto& ts : mITSTPCTracks_segments ){
     LOGP(info,"comparing tracksegments for extraction {} {} first {} last {} counter {}",ts.getCollisionId(),mEventNo,first,last,counter);
     if(ts.getCollisionId()==mEventNo && first==-1){
       first = counter;
